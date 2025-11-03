f0d1240a1a6bf83c8500396e35658599f8804de4
auw2025 <auw@tsss.edu.hk>
2025-11-03 09:59:15 +0800

    center aligned widget

--- lib/widgets/tag_widget.dart
+++ lib/widgets/tag_widget.dart
@@ -8,14 +8,17 @@ class TagWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity, // Expand container horizontally
       padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
       margin: const EdgeInsets.symmetric(vertical: 4.0),
       decoration: BoxDecoration(
         color: Colors.blueGrey,
         borderRadius: BorderRadius.circular(8.0),
       ),
+      alignment: Alignment.center, // Center child within container
       child: Text(
         tag,
+        textAlign: TextAlign.center, // Center text inside Text widget
         style: const TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.bold,
